9199f515044a6599ad8d1f2020c7f23242d26258
cryingwhitecat <root@localhost.localdomain>
2020-02-18 01:37:45 +0200

    fixed public secret key

--- djangodemo/djangodemo/settings.py
+++ djangodemo/djangodemo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k@^6lz$#$f)ooi!(=g0h__1#b5&6buo-u^t*$!k5(sd@)v80ui'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
