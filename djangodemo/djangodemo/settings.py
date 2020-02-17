e623702df2ab062724da1d4ecdf8220eca940f52
cryingwhitecat <root@localhost.localdomain>
2020-02-18 01:57:07 +0200

    migrated to postgres

--- djangodemo/djangodemo/settings.py
+++ djangodemo/djangodemo/settings.py
@@ -76,12 +76,9 @@ WSGI_APPLICATION = 'djangodemo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+db = os.environ.get('DATABASE_URL')
+DATABASES['default'] = dj_database_url.config(default=db)
 
 
 # Password validation
