9e9a7a684627bcd26b94d60cd826f5c01ae647bd
cryingwhitecat <root@localhost.localdomain>
2020-02-18 00:16:46 +0200

    back to debug=true

--- djangodemo/djangodemo/settings.py
+++ djangodemo/djangodemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k@^6lz$#$f)ooi!(=g0h__1#b5&6buo-u^t*$!k5(sd@)v80ui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
